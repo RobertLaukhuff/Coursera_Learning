d20c8ec0a7a9ef7db6e23b7eb49b9256bbeba657
Robert Laukhuff <robert@laukcoder.com>
2021-07-15 13:36:20 -0400

    Added name support to file

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,10 @@
 # Display the ouput
 print("New Python file")
+
+def print_name(first_name, last_name):
+    print("My name is ", firstname, " ", last_name)
+
+name_first = "Robert"
+name_second = "Laukhuff"
+
+print_name(name_first, name_second)
